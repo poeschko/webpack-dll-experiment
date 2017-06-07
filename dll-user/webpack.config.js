ac75b7dfa4a2d3708c0075e17b2e17900442d3d8
Jan Poeschko <jpoeschko@wolfram.com>
2017-06-07 09:16:23 +0200

    initial commit

---
+++ dll-user/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var webpack = require("webpack");
+module.exports = {
+	resolve: {
+		extensions: [".js", ".jsx"]
+	},
+	entry: "./example",
+	output: {
+		path: path.join(__dirname, "js"),
+		filename: "output-[chunkhash].js"
+	},
+	plugins: [
+		new webpack.DllReferencePlugin({
+			context: path.join(__dirname, "..", "dll"),
+			manifest: require("../dll/js/alpha-manifest.json") // eslint-disable-line
+		}),
+		new webpack.DllReferencePlugin({
+			scope: "beta",
+			manifest: require("../dll/js/beta-manifest.json"), // eslint-disable-line
+			extensions: [".js", ".jsx"]
+		})
+	]
+};
